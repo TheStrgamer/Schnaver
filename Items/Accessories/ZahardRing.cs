0b4e3d2ce907403bd53605a30db7ffadba47469f
Konrad <konradseime@gmail.com>
2024-09-07 22:40:44 +0200

    Added Zahard Ring from tower of god

---
+++ Items/Accessories/ZahardRing.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Schnaver.Items.Accessories
+{
+
+	[AutoloadEquip(EquipType.HandsOn)]
+	public class ZahardRing : ModItem
+	{
+		public static readonly float HealthPerSecond = 5f;
+        public static readonly int DefenceBoost = 10;
+
+		public override void SetDefaults() {
+			Item.width = 22;
+			Item.height = 22;
+
+			Item.accessory = true;
+			Item.rare = ItemRarityID.Red;
+			Item.value = Item.buyPrice(gold: 125);
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual) {
+            player.statDefense += DefenceBoost;
+            player.lifeRegen += (int)(HealthPerSecond * 2); 
+
+			player.fireWalk = true; 
+
+		}
+	}
+}
